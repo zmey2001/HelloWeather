c55a4bd88eb0e539651925db8e1e72c88300a98a
zmey2001 <57340897+zmey2001@users.noreply.github.com>
2024-05-06 09:00:10 +0600

    add: Split screen

--- app/src/main/java/ru/fesenko/helloweather/Screens.kt
+++ app/src/main/java/ru/fesenko/helloweather/Screens.kt
@@ -38,6 +38,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import ru.fesenko.helloweather.WeatherUI.WeatherCard
+import ru.fesenko.helloweather.WeatherUI.WeatherStart
 import ru.fesenko.helloweather.network.RetrofitInstance
 import ru.fesenko.helloweather.network.WeatherInfo
 import kotlin.random.Random
@@ -46,60 +47,7 @@ import kotlin.random.Random
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun  Screen2() {
-
-    var weatherInfo by remember {
-        mutableStateOf(
-            WeatherInfo(
-                800,
-                0.0,
-                "",
-                "",
-                "",
-                "",0.toDouble(),
-                "", 0, 0
-            )
-        )
-    }
-
-    val service = RetrofitInstance.create()
-    GlobalScope.launch(Dispatchers.IO) {
-        try {
-            val response = service.getHourlyForecast(
-                "Omsk",
-                "a28824596b88979e3eacd8cedb5171d9"
-            )
-//            displayHourlyForecast(response)
-            val response2 = service.getCurrentWeather( MainActivity.globalVariable.first, MainActivity.globalVariable.second, apiKey = "a28824596b88979e3eacd8cedb5171d9")
-//            displayCurrentWeather(response2)
-            weatherInfo = displayCurrentWeatherView(response2)
-            displayHourlyForecast(response)
-            // Обработка полученного прогноза погоды по часам
-        } catch (e: Exception) {
-            Log.e("Weather", "Error: ${e.message}")
-        }
-    }
-
-    Column(modifier = Modifier.padding(top = 60.dp, start = 10.dp, end = 10.dp)) {
-        Text(
-            text = "Прямо сейчас",
-            fontSize = 45.sp
-        )
-        Text(
-            text = "${weatherInfo.weatherDescription}",
-            fontSize = 35.sp
-        )
-        Text(
-            text = "Ветер ${weatherInfo.windSpeed}",
-            fontSize = 35.sp
-        )
-
-        WeatherCard(weatherInfo)
-        val randomValues = List(20) { Random.nextInt(30,50) }
-        val maxValue = randomValues.maxOrNull() ?: 1
-        Histogram(weatherInfo,randomValues, maxValue)
-
-
-    }
+    WeatherStart()
 }
 
 @SuppressLint("CoroutineCreationDuringComposition")
